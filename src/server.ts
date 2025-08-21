4803d216164eadb2220402337ac8b6be062d8e13
satyajit <satyajit_jbl@yahoo.com>
2025-08-21 23:36:43 +0600

    server error handler done and tested

--- src/server.ts
+++ src/server.ts
@@ -29,4 +29,43 @@ const startServer = async () => {
 
 startServer();
 
+//unhandled rejection error, to prevent server crash/ gracefully shutdown
+process.on("unhandledRejection", (err)=>{
+    console.log("Unhandled Rejection detected ... Server is shutting down", err);
+    server.close(()=>{
+
+    })
+    process.exit(1)
+})
+
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+//uncaught rejection error // not connected with promise // some local problem, unknown var, other symbol
+process.on("uncaughtException", (err)=>{
+    console.log("Uncaught Exception detected ... Server is shutting down", err);
+    server.close(()=>{
+
+    })
+    process.exit(1)
+})
+
+// throw new Error("I forgot to handle this local error")
+
+//signal termination sigterm
+process.on("SIGTERM", (err)=>{
+    console.log("SIGTERM signal received ... Server is shutting down", err);
+    server.close(()=>{
+
+    })
+    process.exit(1)
+})
+
+//for manual shutdown
+process.on("SIGINT", (err)=>{
+    console.log("SIGINT signal received ... Server is shutting down", err);
+    server.close(()=>{
+
+    })
+    process.exit(1)
+})
 
