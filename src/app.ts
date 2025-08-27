153924f3902fb545f0efa7f524a206a7192c241e
satyajit <satyajit_jbl@yahoo.com>
2025-08-28 00:41:59 +0600

    Passport JS and Setting Up Google Cloud Console and Google Authentication Configuration

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,22 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from './app/middlewares/notFound';
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session"
 
 
 const app = express();
-app.use(cookieParser())
-app.use(express.json())
-app.use(cors())
+
+app.use(expressSession({
+    secret: "Your Secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", router)
 
